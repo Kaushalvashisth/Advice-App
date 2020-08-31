9fc1fa1371f35ae08ce627cc2aa45a71a39095ba
KAUSHAL VASHISTH <backspacekaushal@gmail.com>
2020-08-31 23:23:39 +0530

    Updates

---
+++ precache-manifest.0f2ee9d5660db36a62cbd46921c92959.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb2436a337f92355dc6c71fa0a4f742",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52fcd3799168407711c6",
+    "url": "/static/css/main.d7f7f46b.chunk.css"
+  },
+  {
+    "revision": "77447e778d5ae8060758",
+    "url": "/static/js/2.9c9a6ff4.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/static/js/2.9c9a6ff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52fcd3799168407711c6",
+    "url": "/static/js/main.325fbf89.chunk.js"
+  },
+  {
+    "revision": "d6e060dd744ecf58f1d9",
+    "url": "/static/js/runtime-main.9c526786.js"
+  },
+  {
+    "revision": "34311ce842d4b765b047c3fc1245912c",
+    "url": "/static/media/img1.34311ce8.jpg"
+  }
+]);
